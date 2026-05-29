68d74edafe9b43b633fcc0550f244aa04ce4a286
karel <mracek.k@ssmg.cz>
2026-05-29 09:44:36 +0200

    sheet

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 # IMPORTS
 import tkinter as tk
-import settings as s
 import os
+import settings as s
+import pygame
 
 # WINDOW
-WIDTH = 800
-HEIGHT = 600
 bg="skyblue"
 wn = tk.Tk()
-wn.geometry(f"{WIDTH}x{HEIGHT}")
+wn.geometry(f"{s.WIDTH}x{s.HEIGHT}")
 wn.iconphoto(False, tk.PhotoImage(file="assets/lilnig.png"))
 wn.configure(background=bg)
 wn.resizable(False, False)
 wn.title("Best game CZ/SK/UA")
+
+
+
+pygame.mixer.init()
+pygame.mixer.music.load("assets/gameskasoundmain.mp3")
+
+pygame.mixer.music.play()
+
 
 # FUNCTIONS
 def credits():
@@ -22,34 +29,35 @@ def credits():
         os.system("python3 main.py")
     wn2=tk.Tk()
     wn2.iconphoto(False, tk.PhotoImage(file="assets/lilnig.png"))
-    wn2.geometry(f"{WIDTH}x{HEIGHT}")
+    wn2.geometry(f"{s.WIDTH}x{s.HEIGHT}")
     wn2.configure(background=bg)
     wn2.resizable(False, False)
     wn2.title("Best game CZ/SK/UA")
     lblc=tk.Label(wn2, text="CREDITS\nGraphics: Karel Mracek\nProgramming: Ondrej Selucky\nFinger: jew\nHotel: Trivago\nERIK HANYS: TESTOSTERON++", font=("Arial", 20), background=bg)
-    lblc.place(x=WIDTH/5, y=100)
+    lblc.place(x=s.WIDTH/5, y=100)
     okbtn = tk.Button(wn2, text="OK", font=("Arial", 40), width=9,command=ok,background="cyan")
-    okbtn.place(x=WIDTH/5, y=500)
+    okbtn.place(x=s.WIDTH/5, y=500)
 
 def new_game():
     wn.destroy()
+    pygame.mixer.music.stop()
     os.system("python3 game.py")
 
 # MAIN STUFF
 lbl = tk.Label(wn, text="nicolas tovt: the game", font=("Arial", 40), background=bg)
-lbl.place(x=WIDTH/5, y=100)
+lbl.place(x=s.WIDTH/5, y=100)
 
 btn1 = tk.Button(wn, text = "New Game", font=("Arial", 40), width=9,command=new_game,background="cyan")
-btn1.place(x=WIDTH/5, y=200)
+btn1.place(x=s.WIDTH/5, y=200)
 
 btn2 = tk.Button(wn, text = "Load Game", font=("Arial", 40), width=9,state="disabled",background="cyan")
-btn2.place(x=WIDTH/5, y=280)
+btn2.place(x=s.WIDTH/5, y=280)
 
 btn3 = tk.Button(wn, text = "Credits", font=("Arial", 40), width=9,command=credits,background="cyan")
-btn3.place(x=WIDTH/5, y=360)
+btn3.place(x=s.WIDTH/5, y=360)
 
 btn4 = tk.Button(wn, text = "Exit", font=("Arial", 40), width=9,command=wn.destroy,background="cyan")
-btn4.place(x=WIDTH/5, y=440)
+btn4.place(x=s.WIDTH/5, y=440)
 
 # MAIN LOOP
 wn.mainloop()
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 ## Settings, neboli lidove nastaveni
 fullscreen=False
 resolutions=[(800,600),(1280,720),(1366,768),(1920,1080)]
+WIDTH = 800
+HEIGHT = 600
